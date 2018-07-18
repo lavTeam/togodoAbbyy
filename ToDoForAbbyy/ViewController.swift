777c1a45c49e166ca2dee437c2082f9601e7bceb
LavTeam MacMini <zyrexx@yandex.ru>
2018-07-18 15:45:14 +0300

    Initial Commit

---
+++ ToDoForAbbyy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoForAbbyy
+//
+//  Created by Aleksey Larichev on 18.07.2018.
+//  Copyright © 2018 Aleksey Larichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
