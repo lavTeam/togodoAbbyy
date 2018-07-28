16c44382c915f1a1e45d60e7f464707a1ded0863
Aleksey Larichev <lavteam@MacBook-Pro-Aleksey.local>
2018-07-28 15:18:14 +0300

    first commit

---
+++ ToDoForAbbyy/TaskEditorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TaskEditorViewController.swift
+//  ToDoForAbbyy
+//
+//  Created by Aleksey Larichev on 18.07.2018.
+//  Copyright © 2018 Aleksey Larichev. All rights reserved.
+//
+
+import Foundation
